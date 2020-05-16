35276e5271e5725221dba1c3f417551985d8c679
max-davis <max.davis@firstvertical.com>
2020-05-15 23:12:02 -0700

    Updates

--- static/controller.js
+++ static/controller.js
@@ -1,110 +1,112 @@
 var app = angular.module('liftApp', []);
 app.controller('boatLiftController', function($scope, $http) {
     $scope.$on('$viewContentLoaded', function() {
-        $http({
-        url: '/status',
-        method: "GET",
-        headers: {'Content-Type': 'application/json'}
-        }).then(function (result) {
+        $timeout(function() {
+            $http({
+                url: '/status',
+                method: "GET",
+                headers: {'Content-Type': 'application/json'}
+            }).then(function (result) {
 
-        });
+            });
+        },0);
     });
     $scope.upPushed = function() {
         $http({
-        url: '/up',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/up',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.downPushed = function() {
         $http({
-        url: '/down',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/down',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.frontValvesOpen = function() {
         $http({
-        url: '/frontValvesOpen',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/frontValvesOpen',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.frontValvesClose = function() {
         $http({
-        url: '/frontValvesClose',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/frontValvesClose',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.rearValvesOpen = function() {
         $http({
-        url: '/rearValvesOpen',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/rearValvesOpen',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.rearValvesClose = function() {
         $http({
-        url: '/rearValvesClose',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/rearValvesClose',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.masterValveOpen = function() {
         $http({
-        url: '/masterValveOpen',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/masterValveOpen',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.masterValveClose = function() {
         $http({
-        url: '/masterValveClose',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/masterValveClose',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.blowerOn = function() {
         $http({
-        url: '/blowerOn',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/blowerOn',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
     };
     $scope.blowerOff = function() {
         $http({
-        url: '/blowerOff',
-        method: "POST",
-        data: {},
-        headers: {'Content-Type': 'application/json'}
+            url: '/blowerOff',
+            method: "POST",
+            data: {},
+            headers: {'Content-Type': 'application/json'}
         }).then(function (result) {
 
         });
