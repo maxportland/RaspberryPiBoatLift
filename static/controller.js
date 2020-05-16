3b5eb078613fd9fc37a581c520a1120fc930db37
max-davis <max.davis@firstvertical.com>
2020-05-16 16:30:38 -0700

    Updates

--- static/controller.js
+++ static/controller.js
@@ -22,7 +22,7 @@ app.controller('boatLiftController', function($scope, $http, $timeout) {
                     walkRight: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
                     walkLeft: [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
                 },
-                loop: false,
+                loop: true,
                 complete: function(){
 
                 }
