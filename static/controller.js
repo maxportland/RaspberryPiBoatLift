de9b3cf381bac312ea920b42ad5c044263cfd59b
max-davis <max.davis@firstvertical.com>
2020-05-16 19:54:10 -0700

    Updates

--- static/controller.js
+++ static/controller.js
@@ -80,7 +80,7 @@ app.controller('boatLiftController', function($scope, $http, $timeout) {
         });
     };
     $scope.toggleMasterValve = function(event) {
-        var element = $( event.target ).children( ".switch" )[0];
+        var element = angular.element( event.currentTarget ).children( ".switch" )[0];
         if($scope.status.master_valve_state == "open") {
             var size = 30;
             var frame = 0;
