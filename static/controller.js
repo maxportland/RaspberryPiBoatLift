7985af975f72f39fd50edac718c0c8b1e4df2425
max-davis <max.davis@firstvertical.com>
2020-05-16 19:55:43 -0700

    Updates

--- static/controller.js
+++ static/controller.js
@@ -100,7 +100,7 @@ app.controller('boatLiftController', function($scope, $http, $timeout) {
                 $scope.status = result.data;
             });
         } else if($scope.status.master_valve_state == "closed") {
-            toggle_switch.css('background-position', '-300px 0px');
+            element.css('background-position', '-300px 0px');
             var size = 30;
             var frame = -10;
             var interval = setInterval(function() {
