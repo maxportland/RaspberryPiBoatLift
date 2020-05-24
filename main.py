bc79a8f16385151874ad831b5c90dc847625bb30
max-davis <max.davis@firstvertical.com>
2020-05-24 14:11:08 -0700

    Updates

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from flask_endpoints import *
 
 if __name__ == '__main__':
     init_sequence()
-    socketio.run(app, debug=True, host="0.0.0.0", port="8999")
+    socketio.run(app, debug=True, host="0.0.0.0", port="80")
